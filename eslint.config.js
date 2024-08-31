4d24a81071ef31a7f17291f11bd1fecd7f619fb3
Kotaro <kotaro@KotaronoMacBook-Air.local>
2024-08-31 14:36:07 +0900

    eslint-plugin-import追加

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import unusedImports from "eslint-plugin-unused-imports";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
  { ignores: ["dist"] },
@@ -18,10 +19,36 @@ export default tseslint.config(
    "react-hooks": reactHooks,
    "react-refresh": reactRefresh,
    "unused-imports": unusedImports,
+   import: importPlugin,
   },
   rules: {
    ...reactHooks.configs.recommended.rules,
-   "unused-imports/no-unused-imports": "warn", // 3. 未使用のインポートに関するルールを追加
+   "import/order": [
+    "error",
+    {
+     groups: [
+      "builtin", // Node.jsの標準モジュール
+      "external", // 外部モジュール
+      "internal", // プロジェクト内のモジュール（エイリアス使用）
+      "parent", // 親ディレクトリからの相対パス
+      "sibling", // 同じディレクトリからの相対パス
+      "index", // インデックスファイル
+      "unknown", // その他のモジュール
+     ],
+     pathGroups: [
+      {
+       pattern: "{react,react-dom/**,react-router-dom}",
+       group: "builtin",
+       position: "before",
+      },
+     ],
+     pathGroupsExcludedImportTypes: ["builtin"],
+     alphabetize: {
+      order: "asc",
+     },
+    },
+   ],
+   "unused-imports/no-unused-imports": "warn",
    "react-refresh/only-export-components": [
     "warn",
     { allowConstantExport: true },
