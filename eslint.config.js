70f5d7992c7f04db46ffdc7d90384ae7dd067d05
Kotaro <kotaro@KotaronoMacBook-Air.local>
2024-08-31 14:54:06 +0900

    prettierをインストールし、lint:fixの動作確認

--- eslint.config.js
+++ eslint.config.js
@@ -1,58 +1,58 @@
 import js from "@eslint/js";
-import globals from "globals";
+import importPlugin from "eslint-plugin-import";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import unusedImports from "eslint-plugin-unused-imports";
-import importPlugin from "eslint-plugin-import";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
- { ignores: ["dist"] },
- {
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-   ecmaVersion: 2020,
-   globals: globals.browser,
-  },
-  plugins: {
-   "react-hooks": reactHooks,
-   "react-refresh": reactRefresh,
-   "unused-imports": unusedImports,
-   import: importPlugin,
-  },
-  rules: {
-   ...reactHooks.configs.recommended.rules,
-   "import/order": [
-    "error",
-    {
-     groups: [
-      "builtin", // Node.jsの標準モジュール
-      "external", // 外部モジュール
-      "internal", // プロジェクト内のモジュール（エイリアス使用）
-      "parent", // 親ディレクトリからの相対パス
-      "sibling", // 同じディレクトリからの相対パス
-      "index", // インデックスファイル
-      "unknown", // その他のモジュール
-     ],
-     pathGroups: [
-      {
-       pattern: "{react,react-dom/**,react-router-dom}",
-       group: "builtin",
-       position: "before",
-      },
-     ],
-     pathGroupsExcludedImportTypes: ["builtin"],
-     alphabetize: {
-      order: "asc",
-     },
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-   ],
-   "unused-imports/no-unused-imports": "warn",
-   "react-refresh/only-export-components": [
-    "warn",
-    { allowConstantExport: true },
-   ],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "unused-imports": unusedImports,
+      import: importPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // Node.jsの標準モジュール
+            "external", // 外部モジュール
+            "internal", // プロジェクト内のモジュール（エイリアス使用）
+            "parent", // 親ディレクトリからの相対パス
+            "sibling", // 同じディレクトリからの相対パス
+            "index", // インデックスファイル
+            "unknown", // その他のモジュール
+          ],
+          pathGroups: [
+            {
+              pattern: "{react,react-dom/**,react-router-dom}",
+              group: "builtin",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+      "unused-imports/no-unused-imports": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
   },
- }
 );
